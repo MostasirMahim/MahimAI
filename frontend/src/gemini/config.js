db95c476e70184c50e7025d858d9846eab67d2aa
Mostasir Mahim <mahimlive@gmail.com>
2025-08-02 20:56:19 +0600

    Add Gemini Key In React Maually

--- frontend/src/gemini/config.js
+++ frontend/src/gemini/config.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const apiKey = "AIzaSyCa23SrqZccoXRo4vFmPYUYhnRGqvMZ2zQ";
 const genAI = new GoogleGenerativeAI( apiKey );
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });;
 
