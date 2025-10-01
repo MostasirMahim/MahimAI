54c6f69503c38af791997d3461c66a514842123d
Mahim Rahad <66046704+MostasirMahim@users.noreply.github.com>
2025-10-02 05:16:21 +0600

    refactor : Update model in config.js

--- frontend/src/gemini/config.js
+++ frontend/src/gemini/config.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = "AIzaSyDbe3aPpfc17rEkUdbxA8BQUKFs8PUbz9o"; //Its Should be in .env file
 const genAI = new GoogleGenerativeAI( apiKey );
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });;
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });;
 
 
 
